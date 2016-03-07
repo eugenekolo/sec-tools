4bff72792a52ccdd50d078da7ebf74c058422ba4
Eugene Kolo <eugene@kolobyte.com>
2016-03-07 00:38:22 -0500

    Disabling few more installs

--- fuzzers/afl/install-ctf.sh
+++ fuzzers/afl/install-ctf.sh
@@ -1,14 +1,15 @@
-wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
-tar xzf afl-latest.tgz 
-rm afl-latest.tgz
-mv afl-*/ afl
-cd afl
-ls -la 
-make
+#BROKEN ATM
+#wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
+#tar xzf afl-latest.tgz 
+#rm afl-latest.tgz
+#mv afl-*/ afl
+#cd afl
+#ls -la 
+#make
 
-cd qemu_mode
-sudo apt-get install bison libglib2.0-dev
-./build_qemu_support.sh
-cd ../..
+#cd qemu_mode
+#sudo apt-get install bison libglib2.0-dev
+#./build_qemu_support.sh
+#cd ../..
 
-ln -s $PWD/afl/{afl-as,afl-cmin,afl-gcc,afl-gotcpu,afl-plot,afl-qemu-trace,afl-showmap,afl-tmin,afl-whatsup} $CTF_ROOT/bin
+#ln -s $PWD/afl/{afl-as,afl-cmin,afl-gcc,afl-gotcpu,afl-plot,afl-qemu-trace,afl-showmap,afl-tmin,afl-whatsup} $CTF_ROOT/bin
--- fuzzers/pathgrind/install-ctf.sh
+++ fuzzers/pathgrind/install-ctf.sh
@@ -1,5 +1,6 @@
-sudo apt-get -y install libc6-dev-i386 libc6-dev
-git clone  https://github.com/codelion/pathgrind.git
-cd pathgrind
-./install.sh
+#BROKEN ATM
+#sudo apt-get -y install libc6-dev-i386 libc6-dev
+#git clone  https://github.com/codelion/pathgrind.git
+#cd pathgrind
+#./install.sh
 
