6e02a4bdcdda1e04afcd1ae814a881fe778eada0
Eugene Kolo <eugene@kolobyte.com>
2016-03-08 17:51:33 -0500

    Fixing padbuster

--- crypto/padbuster/install-ctf.sh
+++ crypto/padbuster/install-ctf.sh
@@ -1,1 +1,5 @@
-#TODO
+git clone https://github.com/GDSSecurity/PadBuster.git
+perl -MCPAN -e 'install Crypt::SSLeay'
+ln -s $PWD/PadBuster/padBuster.pl $CTF_ROOT/bin/padbuster
+chmod +x $CTF_ROOT/bin/padbuster
+
